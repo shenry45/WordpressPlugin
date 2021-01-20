29ee4e4dabd7be660a1e61173bd48e1e31495017
shenry45 <shawn45henry@gmail.com>
2021-01-20 07:27:31 -0700

    'init'

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+    Plugin Name: Wordpress Development Plugin
+    Description: >-
+    a development plugin to test functionality and implimentation
+    Version: 0.1
+    Author: Shawn Henry
+    License: GPL2
+    */
+
+    
+?>
