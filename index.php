3e16b2967d0e5ba1d20fc322195c12c09a690661
shenry45 <shawn45henry@gmail.com>
2021-01-20 19:27:26 -0700

    add admin menu tab

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
     /*
     Plugin Name: Wordpress Development Plugin
-    Description: >-
-    a development plugin to test functionality and implimentation
+    Description: A development plugin to test functionality and implimentation
     Version: 0.1
     Author: Shawn Henry
     License: GPL2
     */
 
-    
+    add_action( 'admin_menu', 'test_admin');
+    function test_admin() {
+        $page_title = 'Development Testing Menu Page';
+        $menu_title = 'Test Menu';
+        $capabile = 'manage_options';
+        $menu_slug = 'test-menu';
+        $function = 'text_menu_page';
+        $icon_url = 'dashicons-media-code';
+        $position = 75;
+
+        add_menu_page( $page_title, $menu_title, $capabile, $menu_slug, $function, $icon_url, $position);
+    }
 ?>
