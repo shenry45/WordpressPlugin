5e75b36be1aeddb2b1ffe2e438b32a8475180172
shenry45 <shawn45henry@gmail.com>
2021-01-29 20:57:41 -0700

    troubleshooting register_setting with array obj, in progress

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     function test_admin_notice() {
         ?>
         <div class="notice notice-info">
-            <p><?php _e( 'Keep up the good work coding! I believe in you.', 'sample-text-domain' ); ?></p>
+            <p><?php _e( 'Keep up the  good work coding! I believe in you.', 'sample-text-domain' ); ?></p>
         </div>
         <?php
     }
@@ -39,6 +39,19 @@
             <div class="test_admin container">
                 <h1>Yay Sub Menu Page!</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, quidem non tempora officia, optio ut quaerat expedita, tenetur iure quisquam laudantium aliquam vel accusantium nihil quis facere quibusdam aperiam dicta.</p>
+                <form method="post">
+                    <?php settings_fields( 'test_admin_settings' ); ?>
+                    <?php do_settings_sections( 'test_admin_settings' ); ?>
+                    <label for="admin_button_link">Link for redirect</label>
+                    <input type="text" name="admin_button_link"></input
+                    <label for="admin_button_target">What should this button link to?</label>
+                    <select name="admin_button_target">
+                        <option value="_blank">New Window</option>
+                        <option value="_self">Replace the current page</option>
+                        <option value="_parent">New tab at the front of window</option>
+                    </select>
+                    <?php submit_button(); ?>
+                </form>
             </div>
             <?php
         }
@@ -50,9 +63,9 @@
             <div class="test_admin container">
                 <h1>Sample Admin Menu Page</h1>
                 <p class="description">Here we have a display of an admin plugin page created with PHP. The basics of my skills are shown below leveraging actions, filters, and form submission. Please contact me if you like what you see!<br>My email: <a href="mailto:shawn45henry@gmail.com">shawn45henry@gmail.com</a></p>
-                <form method="post" action="options.php">
-                <?php settings_fields( 'test_admin_settings' ); ?>
-                <?php do_settings_sections( 'test_admin_settings' ); ?>
+                <form method="post">
+                    <?php settings_fields( 'test_admin_settings' ); ?>
+                    <?php do_settings_sections( 'test_admin_settings' ); ?>
                     <table class="form-table">
                         <h2>Fill out the Form</h2>
                         <tr valign="top">
@@ -78,18 +91,25 @@
         function update_test_admin() {
             register_setting( 'test_admin_settings', 'test_admin_info' );
             register_setting( 'test_admin_settings', 'test_admin_email' );
-            register_setting( 'test_admin_settings', 'test_admin_date', array(
+            register_setting( 'test_admin_settings', 'test_admin_date' );
+            register_setting( 'general', 'test_admin_extra', array(
                 'type' => 'string',
                 'description' => 'date logged'
                 )
             );
+            register_setting( 'test_admin_settings', 'admin_button_link' );
+            register_setting( 'test_admin_settings', 'admin_button_target' );
         }
     }
 
     /* ALLOW SUBMITTED CONTENT ON FRONT END */
     if ( !function_exists( "test_admin_info" ) ) {
         function test_admin_info($content) {
-            $test_admin = get_option( 'test_admin_info' );
+            $button = array(
+                'link' => get_option( '' ),
+                'target' => get_option( '' )
+            );
+            $test_admin = get_option( 'test_admin_info' ) . get_option( 'test_admin_date ');
             return $content . $test_admin;
         }
         add_filter( 'the_content', 'test_admin_info' );
@@ -98,7 +118,7 @@
     /* CSS INIT */
     if ( !function_exists( "load_admin_style" ) ) {
         function load_admin_style() {
-            wp_register_style( 'test-admin', plugins_url('wordpress-plugin/admin.css') );
+            wp_register_style( 'test-admin', plugins_url( 'wordpress-plugin/admin.css' ) );
             wp_enqueue_style( 'test-admin' );
         }
         add_action( 'admin_enqueue_scripts', 'load_admin_style' );
